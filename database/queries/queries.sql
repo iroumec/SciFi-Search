057695ff2b14425e4aabac78ee67795dd581c1f8
Ulises Velis <uvelis@alumnos.exa.unicen.edu.ar>
2025-09-12 20:28:06 -0300

    Some more scheme tables and their corresponding queries logic

--- database/queries/queries.sql
+++ database/queries/queries.sql
@@ -1,6 +1,9 @@
 -- name: GetUser :one
 SELECT * FROM Users WHERE id = $1;
 
+-- name: GetUserByUsername :one
+SELECT * FROM Users WHERE username = $1;
+
 -- name: ListUsers :many
 SELECT * FROM Users ORDER BY username;
 
@@ -13,8 +16,44 @@ UPDATE Users SET username = $2, email = $3 WHERE id = $1;
 -- name: DeleteUser :exec
 DELETE FROM Users WHERE id = $1;
 
--- name: CreateWork :exec
+-- name: CreateWork :one
 INSERT INTO Works (title,content_type_id,unit,saga_id) VALUES ($1, $2, $3, $4) RETURNING *;
 
--- name: ReviewWork :exec
+-- name: CreateContentType :one
+INSERT INTO ContentTypes (name) VALUES ($1) RETURNING *;
+
+-- name: ReviewWork :one
 INSERT INTO Review (user_id,work_id,score,review,when_watched,liked) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;
+
+-- name: GetLastReview :one
+SELECT * FROM Review WHERE user_id = $1 AND work_id = $2;
+
+-- name: UpdateReview :exec
+UPDATE Review SET score = $2, review = $3, when_watched = $4, liked = $5 WHERE id = $1;
+
+-- name: DeleteReview :exec
+DELETE FROM Review WHERE id = $1;
+
+-- name: ConsumeWork :one
+INSERT INTO ConsumedWorks (user_id,work_id) VALUES ($1,$2) RETURNING *;
+
+-- name: UnconsumeWork :exec
+DELETE FROM ConsumedWorks WHERE user_id = $1 AND work_id = $2;
+
+-- name: LikeWork :one
+INSERT INTO LikedWorks (user_id,work_id) VALUES ($1,$2) RETURNING *;
+
+-- name: LikeReview :one
+INSERT INTO ReviewLike (review_id,user_id) VALUES ($1,$2) RETURNING *;
+
+-- name: UnlikeReview :exec
+DELETE FROM ReviewLike WHERE review_id = $1 AND user_id = $2;
+
+-- name: CommentReview :one
+INSERT INTO ReviewComment (review_id,user_id,comment) VALUES ($1,$2,$3) RETURNING *;
+
+-- name: DeleteReviewComment :exec
+DELETE FROM ReviewComment WHERE id = $1;
+
+-- name: GetConsumedWorksByUser :many
+SELECT * FROM Works w WHERE w.id IN (SELECT id_work FROM ConsumedWorks WHERE id_user = $1) ORDER BY (content_type_id,name);
--- database/schema/schema.sql
+++ database/schema/schema.sql
@@ -1,4 +1,4 @@
--- Creacion de tablas
+--------------------- Creacion de tablas ---------------------
 
 CREATE TABLE IF NOT EXISTS Users (
     id SERIAL PRIMARY KEY,
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS Users (
     name VARCHAR(20) NOT NULL,
     email VARCHAR(50) UNIQUE NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    --password?
 );
 
 CREATE TABLE IF NOT EXISTS Works (
@@ -20,20 +21,23 @@ CREATE TABLE IF NOT EXISTS ContentTypes (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL
 );
+--ContentTypes: Book, Movie, TV Show, Song, Game
+--              Book saga, Movie saga, Related tv shows, Album, Game saga
 
 CREATE TABLE IF NOT EXISTS ConsumedWorks (
     user_id INT, 
     work_id INT,
-    CONSTRAINT pk_consumed_works PRIMARY KEY (user_id,work_id)
-);
+    CONSTRAINT pk_consumedworks PRIMARY KEY (user_id,work_id)
+)
 
---CREATE TABLE AssociatedWorks (
---    id SERIAL PRIMARY KEY,
---    work_id INT REFERENCES Works(id)
---);
+CREATE TABLE IF NOT EXISTS LikedWorks (
+    user_id INT, 
+    work_id INT,
+    CONSTRAINT pk_likedworks PRIMARY KEY (user_id,work_id)
+)
 
 CREATE TABLE IF NOT EXISTS Review (
-    id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY, --puede haber más de una review de la misma obra por el mismo usuario
     user_id INT NOT NULL,
     work_id INT NOT NULL,
     score INT CHECK (score >= 1 AND score <= 10) NOT NULL,
@@ -42,8 +46,36 @@ CREATE TABLE IF NOT EXISTS Review (
     liked BOOLEAN DEFAULT FALSE
 );
 
+CREATE TABLE IF NOT EXISTS ReviewLike (
+    review_id INT, 
+    user_id INT,
+    when_liked TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_reviewlike PRIMARY KEY (review_id,user_id)
+);
 
--- Asignacion de foreign keys
+CREATE TABLE IF NOT EXISTS ReviewComment (
+    id SERIAL PRIMARY KEY,
+    review_id INT,
+    user_id INT,
+    comment VARCHAR(255),
+    when_commented TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+--falta queries.sql a partir de aca
+CREATE TABLE IF NOT EXISTS UserFollows (
+    id_follower INT, 
+    id_followed INT,
+    when_followed TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_userfollows PRIMARY KEY (id_follower,id_followed)
+);
+
+CREATE TABLE IF NOT EXISTS UserFavorites (
+    id_user INT,
+    id_work INT,
+    CONSTRAINT pk_userfavorites PRIMARY KEY (id_user,id_work)
+);
+
+--------------------- Asignacion de foreign keys ---------------------
 
 ALTER TABLE Works ADD CONSTRAINT Works_ContentType
     FOREIGN KEY (content_type_id)
@@ -87,8 +119,65 @@ ALTER TABLE ConsumedWorks ADD CONSTRAINT ConsumedWorks_Works
     INITIALLY IMMEDIATE
 ;
 
--- Funciones + Triggers
+ALTER TABLE LikedWorks ADD CONSTRAINT LikedWorks_Users
+    FOREIGN KEY (user_id)
+    REFERENCES Users(id)
+    NOT DEFERRABLE
+    INITIALLY IMMEDIATE
+;
 
+ALTER TABLE LikedWorks ADD CONSTRAINT LikedWorks_Users
+    FOREIGN KEY (work_id)
+    REFERENCES Works(id)
+    NOT DEFERRABLE
+    INITIALLY IMMEDIATE
+;
+
+ALTER TABLE ReviewLike ADD CONSTRAINT ReviewLike_Review
+    FOREIGN KEY (review_id)
+    REFERENCES Review(id)
+    NOT DEFERRABLE
+    INITIALLY IMMEDIATE
+;
+
+ALTER TABLE ReviewLike ADD CONSTRAINT ReviewLike_User
+    FOREIGN KEY (user_id)
+    REFERENCES Users(id)
+    NOT DEFERRABLE
+    INITIALLY IMMEDIATE
+;
+
+ALTER TABLE UserFollows ADD CONSTRAINT UserFollows_UsersA
+    FOREIGN KEY (id_followed)
+    REFERENCES Users(id)
+    NOT DEFERRABLE
+    INITIALLY IMMEDIATE
+;
+
+ALTER TABLE UserFollows ADD CONSTRAINT UserFollows_UsersB
+    FOREIGN KEY (id_follower)--no se si esto va separado
+    REFERENCES Users(id)
+    NOT DEFERRABLE
+    INITIALLY IMMEDIATE
+;
+
+ALTER TABLE UserFavorites ADD CONSTRAINT UserFavorites_Users
+    FOREIGN KEY (id_user)
+    REFERENCES Users(id)
+    NOT DEFERRABLE
+    INITIALLY IMMEDIATE
+;
+
+ALTER TABLE UserFavorites ADD CONSTRAINT UserFavorites_Works
+    FOREIGN KEY (id_work)
+    REFERENCES Works(id)
+    NOT DEFERRABLE
+    INITIALLY IMMEDIATE
+;
+
+--------------------- Funciones + Triggers ---------------------
+
+--Control para mantener los usernames unicos (tal vez al pedo?)
 CREATE OR REPLACE FUNCTION FN_TRIU_USERNAME()
 RETURNS TRIGGER AS $$
     BEGIN
@@ -108,15 +197,46 @@ CREATE OR REPLACE TRIGGER TRIU_USERNAME
         EXECUTE FUNCTION FN_TRIU_USERNAME();
 
 
-CREATE OR REPLACE FUNCTION FN_TRI_REVIEW()
+-- Carga de la tabla ConsumedWorks al momento de cargar una review
+-- Controla que la obra que se hace la review es unidad
+CREATE OR REPLACE FUNCTION FN_TRIU_REVIEW()
 RETURNS TRIGGER AS $$
     BEGIN
-        INSERT INTO ConsumedWorks VALUES (NEW.user_id,NEW.work_id);
+        IF EXISTS (SELECT 1 FROM Works w WHERE w.id = NEW.work_id AND w.unit) THEN --aca solo entra en inserts, porque no se puede actualizar workid
+            RAISE EXCEPTION 'Solo se puede hacer review de las obras unitarias.';
+        ELSE 
+            IF NEW.liked AND NOT EXISTS (SELECT 1 FROM LikedWorks l WHERE l.user_id = NEW.user_id AND l.work_id = NEW.work_id) THEN 
+                INSERT INTO LikedWorks VALUES (NEW.user_id,NEW.work_id);
+            END IF;
+            IF NOT EXISTS (SELECT 1 FROM Review r WHERE r.user_id = NEW.user_id AND r.work_id = NEW.work_id) THEN
+                INSERT INTO ConsumedWorks VALUES (NEW.user_id,NEW.work_id);
+            END IF;
+        END IF;
         RETURN NEW;
     END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER TRI_REVIEW
-    BEFORE INSERT ON Review 
+CREATE OR REPLACE TRIGGER TRIU_REVIEW
+    BEFORE INSERT OR UPDATE ON Review 
     FOR EACH ROW 
         EXECUTE FUNCTION FN_TRI_REVIEW();
+
+--Eliminar de ConsumedWork debe eliminar de Liked y las review, junto con los comentarios y likes de review
+--PREGUNTAR: tal vez poner un booleano "active" y desactivarlo a modo de eliminar
+--!!ASEGURARSE PONER UN CARTEL DE "¿ESTAS SEGURO?"
+CREATE OR REPLACE FUNCTION FN_TRD_CONSUMEDWORKS()
+RETURNS TRIGGER AS $$
+    BEGIN
+        DELETE FROM ReviewLike lr WHERE OLD.work_id = lr.work_id;
+        DELETE FROM ReviewComment rc WHERE OLD.work_id = lr.work_id;
+        DELETE FROM Review r WHERE r.work_id = OLD.work_id;
+        DELETE FROM LikedWorks lw WHERE lw.work_id = OLD.work_id;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER TRD_CONSUMEDWORKS
+    BEFORE DELETE ON ConsumedWorks
+    FOR EACH ROW 
+        EXECUTE FUNCTION FN_TRD_CONSUMEDWORKS();
+
+--s
