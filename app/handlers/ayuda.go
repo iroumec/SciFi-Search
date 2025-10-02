d210aab66c232d3816915a47889c414741bdd025
iroumec <inaki.roumec@gmail.com>
2025-10-02 09:40:33 -0300

    > PRIMERA ENTREGA <

---
+++ app/handlers/ayuda.go
@@ -0,0 +1,49 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+// ------------------------------------------------------------------------------------------------
+// Registro de los Handlers de Ayuda
+// ------------------------------------------------------------------------------------------------
+
+// Se registran los handlers de ayuda.
+func registrarHandlersAyuda() {
+	rutas := map[string]struct {
+		tmpl string
+		data map[string]any
+	}{
+		"/ayuda": {
+			tmpl: "template/ayuda/ayuda.html",
+		},
+		"/albergue": {
+			tmpl: "template/ayuda/albergue.html",
+			data: map[string]any{
+				"fotos": obtenerFotos("static/img/albergue/"),
+			},
+		},
+		"/carnet-deportivo": {
+			tmpl: "template/ayuda/carnet-deportivo.html",
+		},
+	}
+
+	for ruta, def := range rutas {
+		http.HandleFunc(ruta, handlerTemplate(def.tmpl, def.data))
+	}
+}
+
+// ------------------------------------------------------------------------------------------------
+
+// Handler genérico para GET.
+func handlerTemplate(path string, data map[string]any) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		// Si el método no es GET, error.
+		if r.Method != http.MethodGet {
+			http.Error(w, "Método no permitido.", http.StatusMethodNotAllowed)
+			return
+		}
+		renderizeTemplate(w, path, data, nil)
+	}
+}
