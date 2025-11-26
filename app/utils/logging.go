000587df28165e981898a7ae699cf856b899f47b
iroumec <inaki.roumec@gmail.com>
2025-11-26 17:27:39 -0300

    > TP6 <

---
+++ app/utils/logging.go
@@ -0,0 +1,62 @@
+package utils
+
+// ------------------------------------------------------------------------------------------------
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+// ------------------------------------------------------------------------------------------------
+
+type LoggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+// ------------------------------------------------------------------------------------------------
+
+func NewLoggingResponseWriter(w http.ResponseWriter) *LoggingResponseWriter {
+	// Inicializa el status code por defecto como HTTP 200 OK.
+	return &LoggingResponseWriter{w, http.StatusOK}
+}
+
+// ------------------------------------------------------------------------------------------------
+
+// WriteHeader implementa la interfaz http.ResponseWriter.
+func (lrw *LoggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
+// ------------------------------------------------------------------------------------------------
+
+// Muestra información de logging acerca de las solicitudes entrantes.
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// Tiempo de inicio, para medir la duración de la petición.
+		start := time.Now()
+
+		// Se envuelve el Response Writer para no modificar las acciones
+		// de los middlewares exteriores.
+		wrappedResponseWriter := NewLoggingResponseWriter(w)
+
+		// Se imprime la información de la petición en la consola.
+		// r.Method es el método HTTP (GET, POST, etc.).
+		// r.URL.Path es la ruta solicitada.
+		// r.RemoteAddr es la dirección IP del cliente.
+		log.Printf("--> %s %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
+
+		// Se invoca al siguiente handler en la cadena, el que
+		// realmente trata la solicitud.
+		next.ServeHTTP(wrappedResponseWriter, r)
+
+		// Finalizado el anterior handler, se muestra el tiempo que tardó en
+		// responder la solicitud.
+		log.Printf("<-- %s %s completed in %v", r.Method, r.URL.Path, time.Since(start))
+	})
+}
+
+// ------------------------------------------------------------------------------------------------
