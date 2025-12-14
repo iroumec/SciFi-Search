f8c4684239a5125fa7f2b220ef92d9223fa316cb
iroumec <inaki.roumec@gmail.com>
2025-12-14 15:49:56 -0300

    Se arregló un error que saltaba con struct_manager.go

--- app/utils/struct_manager.go
+++ app/utils/struct_manager.go
@@ -2,6 +2,7 @@ package utils
 
 import "container/list"
 
+// List
 func ListContains(l *list.List, value any) bool {
 	for e := l.Front(); e != nil; e = e.Next() {
 		if e.Value == value {
@@ -12,7 +13,17 @@ func ListContains(l *list.List, value any) bool {
 }
 
 func AddIfNotExists(l *list.List, value any) {
-	if !ListContains(l,value) && ToString(value) != "" {
+	if !ListContains(l, value) && ToString(value) != "" {
 		l.PushFront(value)
 	}
+}
+
+// Array
+func Exists[T comparable](a []T, value T) bool {
+	for _, v := range a {
+		if v == value {
+			return true
+		}
+	}
+	return false
 }
