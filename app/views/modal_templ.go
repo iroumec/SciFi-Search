d2f1709fbc50b3ca5f60cb28bcc83bccac16af03
Ulises Velis <uvelis@alumnos.exa.unicen.edu.ar>
2026-01-14 15:52:19 -0300

    Se estilizó Trends

--- app/views/modal_templ.go
+++ app/views/modal_templ.go
@@ -8,9 +8,6 @@ package views
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-// ------------------------------------------------------------------------------------------------
-// ------------------------------------ Modal Component -------------------------------------------
-// ------------------------------------------------------------------------------------------------
 func ModalRoot() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
@@ -68,7 +65,7 @@ func Modal(modalTitle string, modalBody func() templ.Component) templ.Component 
 		var templ_7745c5c3_Var3 string
 		templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(modalTitle)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `app/views/modal.templ`, Line: 20, Col: 17}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `app/views/modal.templ`, Line: 17, Col: 17}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 		if templ_7745c5c3_Err != nil {
